a93099a683a1bc6c6a2b43fb2c0a37241e834983
Майя Копалина <mika.kopalina@niuitmo.ru>
2025-09-24 16:34:53 +0200

    Initial commit

---
+++ src/main/java/com/example/highloadsystemswardrobemanager/HighloadSystemsWardrobeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.highloadsystemswardrobemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighloadSystemsWardrobeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighloadSystemsWardrobeManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/highloadsystemswardrobemanager/HighloadSystemsWardrobeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.highloadsystemswardrobemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighloadSystemsWardrobeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
