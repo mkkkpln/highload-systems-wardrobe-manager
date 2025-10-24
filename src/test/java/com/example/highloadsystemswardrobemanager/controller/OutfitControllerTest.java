6541b9c77dc3a1cd46fdf7a8c8fea58be8f7b98a
Майя Копалина <mika.kopalina@niuitmo.ru>
2025-10-24 17:24:33 +0200

    refactor(tests): reorganize test structure by domain

--- src/test/java/com/example/highloadsystemswardrobemanager/controller/OutfitControllerTest.java
+++ src/test/java/com/example/highloadsystemswardrobemanager/controller/OutfitControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.highloadsystemswardrobemanager;
+package com.example.highloadsystemswardrobemanager.controller;
 
 import com.example.highloadsystemswardrobemanager.controller.OutfitController;
 import com.example.highloadsystemswardrobemanager.dto.OutfitDto;
